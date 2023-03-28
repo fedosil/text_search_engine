c1c956830131fcaa8b561ad3568003d636de0dbf
fedosil <fedosil@yandex.ru>
2023-03-28 17:11:37 +0300

    add models

--- src/models.py
+++ src/models.py
@@ -1,14 +1,24 @@
-import datetime
+import pandas as pd
+from sqlalchemy import create_engine, Column, Integer, String, ARRAY, MetaData, Table, TIMESTAMP
 
-from sqlalchemy import MetaData, Integer, String, TIMESTAMP, Table, Column, ARRAY
+from src.config import DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME
 
+engine = create_engine(f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
 metadata = MetaData()
 
 document = Table(
     'document',
     metadata,
     Column('id', Integer, autoincrement=True, primary_key=True, index=True),
-    Column('rubrics', ARRAY(String)),
     Column('text', String, nullable=False),
-    Column('created_at', TIMESTAMP),
+    Column('created_date', TIMESTAMP),
+    Column('rubrics', ARRAY(String)),
 )
+
+metadata.create_all(engine)
+
+df = pd.read_csv('posts.csv')
+df['rubrics'] = df['rubrics'].apply(lambda x: [r.strip()[1:-1] for r in x[1:-1].split(',')])
+df = df.where(pd.notnull(df), None)
+
+df.to_sql('document', engine, if_exists='append', index=False, method='multi')
