95fa4cd4fe7b62a65911da97cd59e01cad712c71
fedosil <fedosil@yandex.ru>
2023-03-31 16:41:50 +0300

    add tests

---
+++ src/main.py
@@ -0,0 +1,37 @@
+from elasticsearch import NotFoundError
+from fastapi import FastAPI, Depends
+
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi.responses import JSONResponse
+
+from src.database import get_async_session
+from src.models import document, es, index_name, es_base_client, Document
+
+app = FastAPI()
+
+
+@app.get("/document/{text}")
+async def document_search(text: str, session: AsyncSession = Depends(get_async_session)):
+    resp = es.submit(index=index_name, q=text, default_operator='AND', df='text', size=10000)
+    id_list = [c['_source']['id'] for c in resp['response']['hits']['hits']]
+    print(len(id_list))
+    if id_list:
+        query = select(document).filter(document.c.id.in_(id_list)).order_by(document.c.created_date).limit(20)
+        result = await session.execute(query)
+        return result.all()
+    return JSONResponse(status_code=404, content={"message": "Not Found"})
+
+
+@app.delete('/document/{id}')
+async def document_delete(_id: int, session: AsyncSession = Depends(get_async_session)):
+    try:
+        es_base_client.delete(index=index_name, id=_id)
+        query = select(document).filter(document.c.id == _id)
+        result = await session.execute(query)
+        obj = await session.get(Document, _id)
+        await session.delete(obj)
+        await session.commit()
+        return {'message': 'Document deleted', 'document': result.all()}
+    except NotFoundError:
+        return JSONResponse(status_code=404, content={"message": "Document not found"})
